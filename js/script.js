1896358d63475e421aa7e8d03bead672e65aa62b
Anthony Samuel Freitas <84025181+sammyfreitas@users.noreply.github.com>
2023-11-07 21:28:38 -0300

    PRIMEIRO COMMIT

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementByID("botao_light").addEventListener("click", function(){
+	LINK REL=“STYLESHEET” TYPE=“TEXT/CSS” HREF=“STYLE.CSS”
+}
+
+document.getElementByID("botao_dark").addEventListener("click", function(){
+	LINK REL=“STYLESHEET” TYPE=“TEXT/CSS” HREF=“dark.CSS”
+}
+
+
+document.getElementByID("botao_barbie").addEventListener("click", function(){
+	LINK REL=“STYLESHEET” TYPE=“TEXT/CSS” HREF=“barbie.CSS”
+}
